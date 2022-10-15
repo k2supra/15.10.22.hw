8e48658cfbb84b73618c2b355ac3fabbbdde25ea
k2supra <andrijkorolevic@gmail.com>
2022-10-15 13:30:36 +0300

    Created new feature/hw_ex_2

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("ABOBA != OBEME")
+while True:
+    try:
+        print("Введіть 1 для старту")
+        num = int(input("->"))
+
+        if num == 1:
+            for i in range(1, 11):
+                for j in range(1, 11):
+                    print(f"{i} * {j} = {i * j}", end="\t")
+                print()
+            break
+        else:
+            print("ВВЕДІТЬ 1 ДЛЯ ПОЧАТКУ")
+
+    except Exception as ex:
+        print(f'Error', {ex})
